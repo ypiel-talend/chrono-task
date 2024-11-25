aa9f8d2088343ae57fe9be660fb5cab6a2548d09
Yves Piel <ypiel@talend.com>
2024-11-25 08:58:08 +0100

    Reverse order of duration table view.

--- src/main/java/com/github/ypiel/chronotask/control/DurationByDateTableView.java
+++ src/main/java/com/github/ypiel/chronotask/control/DurationByDateTableView.java
@@ -1,7 +1,5 @@
 package com.github.ypiel.chronotask.control;
 
-import com.github.ypiel.chronotask.model.Task;
-
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -16,37 +14,38 @@ import javafx.scene.control.cell.TextFieldTableCell;
 import static com.github.ypiel.chronotask.model.Task.DurationByDate;
 
 public class DurationByDateTableView extends TableView<DurationByDate> {
-public DurationByDateTableView() {
-    super();
+    public DurationByDateTableView() {
+        super();
 
-    TableColumn<DurationByDate, String> dateTableColumn = new TableColumn<>("Date");
-    dateTableColumn.setCellValueFactory(cellData -> {
-        LocalDate date = cellData.getValue().getDate();
-        return new SimpleStringProperty(date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-    });
+        TableColumn<DurationByDate, String> dateTableColumn = new TableColumn<>("Date");
+        dateTableColumn.setCellValueFactory(cellData -> {
+            LocalDate date = cellData.getValue().getDate();
+            return new SimpleStringProperty(date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        });
 
-    TableColumn<DurationByDate, String> durationTableColumn = new TableColumn<>("Durée");
-    durationTableColumn.setCellValueFactory(cellData -> {
-        Duration duration = cellData.getValue().getDuration();
-        long hours = duration.toHours();
-        long minutes = duration.toMinutes() % 60;
-        long seconds = duration.toSeconds() % 60;
-        return new SimpleStringProperty(String.format("%d:%d:%s", hours, minutes, seconds));
-    });
+        TableColumn<DurationByDate, String> durationTableColumn = new TableColumn<>("Durée");
+        durationTableColumn.setCellValueFactory(cellData -> {
+            Duration duration = cellData.getValue().getDuration();
+            long hours = duration.toHours();
+            long minutes = duration.toMinutes() % 60;
+            long seconds = duration.toSeconds() % 60;
+            return new SimpleStringProperty(String.format("%d:%d:%s", hours, minutes, seconds));
+        });
 
-    TableColumn<DurationByDate, String> notesColumn = new TableColumn<>("Notes");
-    notesColumn.setCellValueFactory(new PropertyValueFactory<>("notes"));
-    notesColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-    notesColumn.setOnEditCommit(event -> {
-        DurationByDate DurationByDate = event.getRowValue();
-        DurationByDate.setNotes(event.getNewValue());
-    });
+        TableColumn<DurationByDate, String> notesColumn = new TableColumn<>("Notes");
+        notesColumn.setCellValueFactory(new PropertyValueFactory<>("notes"));
+        notesColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        notesColumn.setOnEditCommit(event -> {
+            DurationByDate DurationByDate = event.getRowValue();
+            DurationByDate.setNotes(event.getNewValue());
+        });
 
-    this.getColumns().addAll(dateTableColumn, durationTableColumn, notesColumn);
-    this.setEditable(true);
-}
+        this.getColumns().addAll(dateTableColumn, durationTableColumn, notesColumn);
+        this.setEditable(true);
+    }
 
     public void setDurationsByDate(List<DurationByDate> durationByDateList) {
+        durationByDateList.sort((o1, o2) -> o2.getDate().compareTo(o1.getDate()));
         this.setItems(FXCollections.observableArrayList(durationByDateList));
     }
 }
